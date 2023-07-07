e7f9ecf5591a3852370cd5cdcc243d35c3ed76ee
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-07-06 22:06:42 -0400

    update on about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import jumpVideo from '../media/jump.mp4'
+import holdVideo from '../media/hold.mp4'
+import chillVideo from '../media/chill.mp4'
 
 const About = ({ isDarkMode }) => {
     return (
         <div className={`page ${isDarkMode ? 'dark' : ''}`}>
-            <h2>About</h2>
+            <div className="grid-container">
+                <div className="grid-item full-width">
+                    <video src={jumpVideo} autoPlay loop muted />
+                </div>
+                <div className="grid-item full-width">
+                    <h2>Let's Jump In !</h2>
+                </div>
+                <div className="grid-item full-width">
+                    <p>Text 1...</p>
+                </div>
+                <div className="grid-item text">
+                    <p>Text 2...</p>
+                </div>
+                <div className="grid-item two-thirds">
+                    <video src={holdVideo} autoPlay loop muted />
+                </div>
+                <div className="grid-item full-width">
+                    <p>Text 3...</p>
+                </div>
+                <div className="grid-item text">
+                    <p>Text 4...</p>
+                </div>
+                <div className="grid-item two-thirds">
+                    <video src={chillVideo} autoPlay loop muted />
+                </div>
+                <div className="grid-item full-width">
+                    <p>Text 5...</p>
+                </div>
+            </div>
         </div>
     )
 }
